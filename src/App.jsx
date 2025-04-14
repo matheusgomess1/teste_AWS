690ee20ee8edf777c0ade3304d17c4c30ed5ff31
Matheus Gomes <jmdeus00@gmail.com>
2025-04-14 17:40:38 -0300

    primeiro

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "2rem",
+      fontFamily: "Arial, sans-serif",
+      textAlign: "center",
+      backgroundColor: "#f0f8ff",
+      color: "#000000"  // 👈 cor do texto preta
+    }}>
+      🌟 Acredite em você. Cada passo te leva mais perto do seu sonho!
+    </div>
+  );
+}
+
+export default App;
